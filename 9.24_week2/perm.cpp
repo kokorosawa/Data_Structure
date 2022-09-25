3c7cf7a2311d64583c917536ec34453ecae3fb97
kokorosawa <matt920404@gmail.com>
2022-09-25 12:22:59 +0800

    hw

---
+++ 9.24_week2/perm.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+void tobit(int a[], int n, int bit){
+    int i = bit - 1;
+    while (n > 0)   
+    {
+        a[i] = n % 2;   
+        i = i - 1;
+        n = n / 2;
+    }
+    for(int i = 0; i < bit; i++)
+        printf("%d",a[i]);
+    printf("\n");
+    
+    
+}
+
+void perm(int a[],int s,int e,int i, int bit)
+{
+    if(i == e) return;
+    tobit(a, i, bit);
+    return perm(a, s, e, i + 1, bit);
+}
+
+int main()
+{
+    int bit;
+    scanf("%d", &bit);
+    int *a =  (int *)malloc(sizeof(int) * bit);
+    memset(a, 0, bit * sizeof(a[0]));
+    perm(a, 0, pow(2,bit),0 , bit);
+}
+
